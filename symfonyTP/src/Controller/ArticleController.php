ee8b820d3777f60af64f4034ac4592befa4e3fa9
Bryce <bryce.tavares@gmail.com>
2019-04-16 09:52:24 +0200

    ajout des composers form et annotations

---
+++ symfonyTP/src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    public function addArticle(Request $request): Response
+    {
+        $isOk = false;
+    }
+}
