525a4136efbdf3a3bd9151bed9a5d018d85c5701
Bryce <bryce.tavares@gmail.com>
2019-04-16 14:39:26 +0200

    ajout de doctrine et asset et début code article

--- symfonyTP/src/Controller/ArticleController.php
+++ symfonyTP/src/Controller/ArticleController.php
@@ -1,16 +1,49 @@
 <?php
-
+declare(strict_types = 1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ArticleController
+ * @package App\Controller
+ * @Route(path="/article")
+ */
 class ArticleController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route(path="/add")
+     */
     public function addArticle(Request $request): Response
     {
         $isOk = false;
+        //$newArticleForm = $this->createForm(ArticleType::class);
+//        $newArticleForm->handleRequest($request);
+//        if($newArticleForm->isSubmitted() && $newArticleForm->isValid()){
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($newArticleForm->getData());
+//        }
+        return $this->render('Article/add.html.twig', ['isOk' => $isOk]);
+    }
+
+
+    /*public function delete(Article $article): Response
+    {
+
+    }*/
+
+    /**
+     * @return Response
+     * @Route(path="/list")
+     */
+    public function list(): Response
+    {
+
     }
 }
