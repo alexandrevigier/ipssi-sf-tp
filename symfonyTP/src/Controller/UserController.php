cc18e3285c550503c2a64549143ce703eeacfd16
Dozmare <libegon78@hotmail.fr>
2019-04-18 09:44:14 +0200

    add user

---
+++ symfonyTP/src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+namespace App\Controller;
+use App\Entity\Users;
+use App\Form\UserType;
+use App\Repository\UsersRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+/**
+ * @Route(path="/users")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route(path="/add")
+     */
+    public function add(Request $request): Response
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $player = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($player);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_user_add', [
+                'id' => $player->getId(),
+            ]);
+        }
+
+        return $this->render('Users/add.html.twig', [
+            'UserType' => $form->createView()
+        ]);
+    }
+}
---
+++ symfonyTP/src/Form/UserType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mail')
+            ->add('password')
+            ->add('name')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
