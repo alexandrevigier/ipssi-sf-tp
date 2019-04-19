500ab80808e86dc2e96f98fd17b9faf73f0332c7
Dozmare <libegon78@hotmail.fr>
2019-04-19 16:03:26 +0200

    fix warning

--- src/Migrations/Version20190419121448.php
+++ src/Migrations/Version20190419121448.php
@@ -21,15 +21,15 @@ final class Version20190419121448 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.');
+        $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+        'Migration can only be executed safely on \'mysql\'.' );
 
         $this->addSql('CREATE TABLE commentaire (
-                      id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL,
-                      user_id INT DEFAULT NULL, commentaire VARCHAR(255) NOT NULL,
-                      INDEX IDX_67F068BC7294869C (article_id),
-                      INDEX IDX_67F068BCA76ED395 (user_id),
-                      PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+                  id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL,
+                  user_id INT DEFAULT NULL, commentaire VARCHAR(255) NOT NULL,
+                  INDEX IDX_67F068BC7294869C (article_id),
+                  INDEX IDX_67F068BCA76ED395 (user_id),
+                  PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (
                       id INT AUTO_INCREMENT NOT NULL, mail VARCHAR(255) NOT NULL,
                       password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, admin SMALLINT NOT NULL,
@@ -46,7 +46,7 @@ final class Version20190419121448 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.');
+        'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BCA76ED395');
         $this->addSql('DROP TABLE commentaire');
